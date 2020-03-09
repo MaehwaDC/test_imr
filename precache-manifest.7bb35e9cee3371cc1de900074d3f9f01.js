a032e89ec9ccaeafb3577f877d7dfa61acfddefc
Gennady Voznyuk <voznyuk.gennady@gmail.com>
2020-03-09 15:58:06 +0300

    Updates

---
+++ precache-manifest.7bb35e9cee3371cc1de900074d3f9f01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf67d579884a0b2e1a989567a8bd2f4",
+    "url": "/test_imr/index.html"
+  },
+  {
+    "revision": "99252d2e8aac44571584",
+    "url": "/test_imr/static/css/main.0eff996d.chunk.css"
+  },
+  {
+    "revision": "c98e1c51569323bf7cc9",
+    "url": "/test_imr/static/js/2.97ee1618.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/test_imr/static/js/2.97ee1618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99252d2e8aac44571584",
+    "url": "/test_imr/static/js/main.7f7cbe8d.chunk.js"
+  },
+  {
+    "revision": "fa26918a33a4a890c787",
+    "url": "/test_imr/static/js/runtime-main.cfd3f042.js"
+  }
+]);
