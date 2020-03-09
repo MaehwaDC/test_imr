3518daa256ebe10a4ddd32b36d2bdc2c8beacb3e
Gennady Voznyuk <voznyuk.gennady@gmail.com>
2020-03-09 12:34:14 +0300

    add rating

--- src/ui-kit/Rating/Rating.js
+++ src/ui-kit/Rating/Rating.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
-const Rating = ({ rating, className }) => {
-  return <div className={classNames('rating', className)}></div>;
+import { random } from '../../utils/helpers';
+import { Star } from './Star';
+
+import './styles/Rating.scss';
+
+const Rating = ({ rating, heightRating, className }) => {
+  const starts = Array.from(new Array(heightRating), () => (
+    <Star key={random()} pracentage={heightRating / rating} />
+  ));
+  return (
+    <div className={classNames('rating', className)}>
+      <div className="rating__stars-wrapper">{starts}</div>
+      <span className="rating__count">{rating.toFixed(1)}</span>
+    </div>
+  );
 };
 
-export default Rating;
+Rating.propTypes = {
+  rating: PropTypes.number,
+  heightRating: PropTypes.number,
+};
+
+Rating.defaultProps = {
+  rating: 5,
+  heightRating: 5,
+};
+
+export default React.memo(Rating);
